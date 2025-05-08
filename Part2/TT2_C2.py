a496f47a8718714f05368ecb41edb90f6bc2f8ab
HungBiBo3908 <HungBiBo3908@users.noreply.github.com>
2025-05-08 23:34:32 +0700

    Fix TT1_C2
    sửa lỗi TT1_c2 và hoàn thiện TT2_c2

---
+++ Part2/TT2_C2.py
@@ -0,0 +1,16 @@
+#phân tích thừa số nguyên tố
+import TT1_C2
+from math import sqrt
+
+def factor(number):
+    lst = TT1_C2.Eratos2(int(sqrt(number)))
+    lst.reverse()
+    list_factor = []
+    for i in lst:
+        while number % i == 0:
+            list_factor.append(i)
+            number = number/i
+        if number == 1:
+            break
+    print(list_factor)
+
