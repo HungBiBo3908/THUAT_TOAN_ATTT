b8f4fb482f5f27433cd9bbd3eff22f23cfca81cd
HungBiBo3908 <HungBiBo3908@users.noreply.github.com>
2025-05-09 11:53:09 +0700

    Update TT2_C2.py
    sửa nốt logic c2.2. cần thêm case để kiểm tra full

--- Part2/TT2_C2.py
+++ Part2/TT2_C2.py
@@ -1,8 +1,9 @@
 #phân tích thừa số nguyên tố
 import TT1_C2
+from math import sqrt
 
 def factor(number):
-    lst = TT1_C2.Eratos2((number))
+    lst = TT1_C2.Eratos2(int(sqrt(number)))
     lst.reverse()
     list_factor = []
     for i in lst:
@@ -11,5 +12,6 @@ def factor(number):
             number = number/i
         if number == 1:
             break
+    if number != 1: list_factor.append(int(number))
     return list_factor
-
+print(factor(223556))
