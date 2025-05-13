d639257a5c558af6f1e856d59f794753a0c082e3
HungBiBo3908 <HungBiBo3908@users.noreply.github.com>
2025-05-13 11:39:02 +0700

    tăng tốc eratos2
    Chuyển mảng phân đoạn sang dạng bool

--- Part2/TT1_C2.py
+++ Part2/TT1_C2.py
@@ -13,22 +13,28 @@ def Eratos(last):
     return lst_priv
 
 def Eratos2(last):
+    if last == 2:
+        return [2]
+    if last == 3:
+        return [2,3]
+    if last < 2:
+        return []
     chia_list = int(sqrt(last))
     c = Eratos(chia_list-1)
     for i in range (chia_list,last+1,chia_list): #chạy từng cặp (chia_list) số từ 0 đến last
         check_max = i+chia_list
-        if check_max > last: check_max = last+1
-        a = [t for t in range(i,check_max)] #1 mảng chỉ chứa 1 bộ chia_list số liên tục
+        if check_max > last: check_max = last + 1
+        a = [True] * (check_max - i)#1 mảng chỉ chứa 1 bộ chia_list số liên tục
         check_q = int(sqrt(check_max - 1))
         for j in c:
             if j <= check_q: 
                 m = int((check_max-1)/j)
                 n = ceiling(i/j)
                 for k in range(n,m+1): #chặn đầu đít của bội số nguyên tố trong bộ số a.
-                    if a[j*k-i] != 0:
-                        a[j*k-i] = 0 
+                    if a[j*k-i] != False:
+                        a[j*k-i] = False 
             else: break
-        for j in a:
-            if j != 1 and j!= 0:
-                c.append(j)
+        for j in range(check_max - i):
+            if a[j] == True:
+                c.append(i+j)
     return c
